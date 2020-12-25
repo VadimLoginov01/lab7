05978ae4ed22edc49704a63165d0a5a7fb43537d
VadimLoginov01 <vadlog2001@mail.ru>
2020-12-26 01:02:13 +0300

    release v1

---
+++ lab3/index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Laba3;
+
+use Loginov\MyLog;
+
+require_once "core\EquationInterface.php";
+require_once "core\LogAbstract.php";
+require_once "core\LogInterface.php";
+require_once "loginov\Line.php";
+require_once "loginov\MyLog.php";
+require_once "loginov\Quadratic.php";
+MyLog::log("vadim");
+MyLog::write();
